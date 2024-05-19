b4ba7d4a581aba70e91a8554002fbcabb489be73
petrsabatka <petr.sabatka@gooddata.com>
2024-05-19 22:12:25 +0200

    another comment [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // Comment
+// add another commet
 
 describe('MainContent', () => {
   it('should render a button', () => {
